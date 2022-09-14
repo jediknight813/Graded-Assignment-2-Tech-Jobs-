ee3c96e0969aec46f83ea49282bed82e44d10096
-THE-LAST-SAMURAI- <connor813@outlook.com>
2022-09-14 12:56:08 -0500

    cleaned up code

--- src/main/java/org/launchcode/techjobs/oo/JobField.java
+++ src/main/java/org/launchcode/techjobs/oo/JobField.java
@@ -1,4 +1,47 @@
 package org.launchcode.techjobs.oo;
 
+import java.util.Objects;
+
 public abstract class JobField {
+
+    public int id;
+    public static int nextId = 1;
+    public String value;
+
+    public JobField() {
+        id = nextId;
+        nextId++;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof JobField)) return false;
+        JobField jobField = (JobField) o;
+        return getId() == jobField.getId();
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+
 }
